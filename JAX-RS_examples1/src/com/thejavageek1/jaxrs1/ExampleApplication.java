dda9e690b7668125b78815b05083739eae0f100a
Maria Bajena <mbajena@vp.pl>
2019-06-10 19:30:58 +0200

    Remove unused files

--- JAX-RS_examples1/src/com/thejavageek1/jaxrs1/ExampleApplication.java
+++
@@ -1,9 +0,0 @@
-package com.thejavageek1.jaxrs1;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
- 
-@ApplicationPath("/rest")
-public class ExampleApplication extends Application {
- 
-}
