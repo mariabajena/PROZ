6687d9c08d157089e801a089db1ec369cfa19f0f
Maria Bajena <bajena87@gmail.com>
2019-06-10 17:30:54 +0200

    Add files via upload

---
+++ JAX-RS_examples1/src/com/thejavageek1/jaxrs1/ExampleApplication.java
@@ -0,0 +1,9 @@
+package com.thejavageek1.jaxrs1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/rest")
+public class ExampleApplication extends Application {
+ 
+}
